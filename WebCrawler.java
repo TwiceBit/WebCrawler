cd0b905a32b958851dc5757cb3480709a8b187ae
TwiceBit <?>
2018-08-18 20:01:49 +0200

    Adding SSL Support

--- WebCrawler.java
+++ WebCrawler.java
@@ -1,16 +1,23 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.net.ssl.HttpsURLConnection;
+
 public class WebCrawler {
 
+	// HTTP
+	
 	public static Object[] Crawl(String url) {
 		try {
 			URL u = new URL(url);
-			InputStream in = u.openStream();
+			HttpURLConnection con = (HttpURLConnection) u.openConnection();
+			con.setRequestProperty("user-agent", "Java-Bot");
+			InputStream in = con.getInputStream();
 			InputStreamReader r = new InputStreamReader(in);
 			ArrayList<Character> list = new ArrayList();
 			while (r.ready()) {
@@ -27,7 +34,9 @@ public class WebCrawler {
 		URL u;
 		try {
 			u = new URL(url);
-			InputStream in = u.openStream();
+			HttpURLConnection con = (HttpURLConnection) u.openConnection();
+			con.setRequestProperty("user-agent", "Java-Bot");
+			InputStream in = con.getInputStream();
 			return in;
 		} catch (IOException e) {
 			
@@ -41,7 +50,9 @@ public class WebCrawler {
 		URL u;
 		try {
 			u = new URL(url);
-			InputStream in = u.openStream();
+			HttpURLConnection con = (HttpURLConnection) u.openConnection();
+			con.setRequestProperty("user-agent", "Java-Bot");
+			InputStream in = con.getInputStream();
 			ArrayList<String> list = new ArrayList<>();
 			Scanner src = new Scanner(in);
 			while (src.hasNextLine()) {
@@ -60,7 +71,9 @@ public class WebCrawler {
 		URL u;
 		try {
 			u = new URL(url);
-			InputStream in = u.openStream();
+			HttpURLConnection con = (HttpURLConnection) u.openConnection();
+			con.setRequestProperty("user-agent", "Java-Bot");
+			InputStream in = con.getInputStream();
 			ArrayList<String> list = new ArrayList<>();
 			Scanner src = new Scanner(in);
 			while (src.hasNextLine()) {
@@ -74,5 +87,88 @@ public class WebCrawler {
 		return null;
 
 	}
+	
+	
+	//  HTTPS
+	
+	
+	public static Object[] SSLCrawl(String url) {
+		try {
+			URL u = new URL(url);
+			HttpsURLConnection con = (HttpsURLConnection) u.openConnection();
+			con.setRequestProperty("user-agent", "Java-Bot");
+			InputStream in = con.getInputStream();
+			
+			InputStreamReader r = new InputStreamReader(in);
+			ArrayList<Character> list = new ArrayList();
+			while (r.ready()) {
+				list.add((char) r.read());
+			}
+			return list.toArray();
+		} catch (Exception e) {
+
+		}
+		return null;
+	}
+
+	public static InputStream SSLgetInputStream(String url) {
+		URL u;
+		try {
+			u = new URL(url);
+			HttpsURLConnection con = (HttpsURLConnection) u.openConnection();
+			con.setRequestProperty("user-agent", "Java-Bot");
+			InputStream in = con.getInputStream();
+			return in;
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+
+	public static String[] SSLgetStringArray(String url) {
+		URL u = null;
+		try {
+			u = new URL(url);
+			HttpsURLConnection con = (HttpsURLConnection) u.openConnection();
+			con.setRequestProperty("user-agent", "Java-Bot");
+			InputStream in = con.getInputStream();
+			ArrayList<String> list = new ArrayList<>();
+			Scanner src = new Scanner(in);
+			while (src.hasNextLine()) {
+				list.add(src.nextLine());
+			}
+			return (String[]) list.toArray();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+
+	public static ArrayList<String> SSLgetListCode(String url) {
+		URL u;
+		try {
+			u = new URL(url);
+			HttpsURLConnection con = (HttpsURLConnection) u.openConnection();
+			con.setRequestProperty("user-agent", "Java-Bot");
+			InputStream in = con.getInputStream();
+			ArrayList<String> list = new ArrayList<>();
+			Scanner src = new Scanner(in);
+			while (src.hasNextLine()) {
+				list.add(src.nextLine());
+			}
+			return list;
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+	
+	
 
 }
